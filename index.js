cdadfca7ae0be5a64cb9f88aee317bc87badc4f4
Leonardo Matos <leomp120894@gmail.com>
2017-12-15 11:35:23 -0200

    Update index.js

--- index.js
+++ index.js
@@ -1,110 +1,131 @@
-var storeID
+'use strict'
 
-// GET STORE ID
-function init (storeId, logger) {
-  storeID = storeId
-  if (typeof logger === 'object' && logger.hasOwnProperty('log') && logger.hasOwnProperty('error')) {
-    console = logger
-  }
-}
-
-const isNodeJS = false
-const https
+var isNodeJs = false
 // Verify if the script is Node JS
 if (typeof module !== 'undefined' && module.exports) {
-  isNodeJS = true
-  https = require('https')
+  isNodeJs = true
 }
 
-// Function to run function by endpoint and method
-function runMethod (callback, endpoint, host, method) {
-  let path
-  if (!host) {
-    host = 'api.e-com.plus'
-    path = '/v1'
-  } else if (host !== 'api.e-com.plus') {
-    path = '/api/v1'
+var EcomIo = function () {
+  let storeId, https, logger
+
+  if (isNodeJs) {
+    https = require('https')
   }
-  if (!method) {
-    method = 'GET'
-  }
-  
-  if (isNodeJS === true) {
-    const options = {
-      hostname: host,
-      path: path + endpoint,
-      method: method,
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Store-ID': storeID
-      }
+
+  // Function to run function by endpoint and method
+  let runMethod = function (callback, endpoint, host, method) {
+    let path
+    if (!host) {
+      host = 'api.e-com.plus'
+      path = '/v1'
+    } else if (host !== 'api.e-com.plus') {
+      path = '/api/v1'
     }
-
-    const req = https.request(options, (res) => {
-      if (res.statusCode !== 200) {
-        // TODO: error handling
-        let err = new Error('Request failed trying to get store info\nStatus code: ' + res.statusCode)
-        console.error(err)
-        // consume response data to free up memory
-        res.resume()
-        return
-      }
-
-      let rawData = ''
-      res.setEncoding('utf8')
-      res.on('data', (chunk) => { rawData += chunk })
-      res.on('end', () => {
-        try {
-          let parsedData = JSON.parse(rawData)
-        } catch (e) {
-          console.error(e)
-        }
-      })
-      req.on('error', function (error) {
-        console.error('problem with request:' + error.message)
-      })
-      req.end()
-    })
-  } else {
-    let ajax = new XMLHttpRequest()
-    let url = 'https://' + host + path + endpoint
-    ajax.open(method, url, true)
-    ajax.send()
-    ajax.onreadystatechange = function () {
-      if (ajax.readyState === 4 && ajax.status === 200) {
-        let body = JSON.parse(ajax.responseText)
-        if (typeof callback === 'function') {
-          callback(body)
-        } else {
-          return body
+    if (!method) {
+      method = 'GET'
+    }
+    
+    if (isNodeJs === true) {
+      const options = {
+        hostname: host,
+        path: path + endpoint,
+        method: method,
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Store-ID': storeId
         }
       }
+
+      const req = https.request(options, function (res) {
+        if (res.statusCode !== 200) {
+          // TODO: error handling
+          let err = new Error('Request failed trying to get store info\nStatus code: ' + res.statusCode)
+          logger.error(err)
+          // consume response data to free up memory
+          res.resume()
+          return
+        }
+
+        let rawData = ''
+        res.setEncoding('utf8')
+        res.on('data', function (chunk) { rawData += chunk })
+        res.on('end', function () {
+          try {
+            let body = JSON.parse(rawData)
+            if (typeof callback === 'function') {
+              callback(body)
+            } else {
+              return body
+            }
+          } catch (e) {
+            logger.error(e)
+          }
+        })
+        req.on('error', function (error) {
+          logger.error('problem with request:' + error.message)
+        })
+        req.end()
+      })
+    } else {
+      let ajax = new XMLHttpRequest()
+      let url = 'https://' + host + path + endpoint
+      ajax.open(method, url, true)
+      ajax.send()
+      ajax.onreadystatechange = function () {
+        if (ajax.readyState === 4 && ajax.status === 200) {
+          let body = JSON.parse(ajax.responseText)
+          if (typeof callback === 'function') {
+            callback(body)
+          } else {
+            return body
+          }
+        }
+      }
     }
   }
-}
 
-// Function to get ID product by sku
-function getProductBySku (sku) {
-  let endpoint = '/products.json?sku=' + sku
-  let method = 'GET'
-  let response = runMethod(endpoint, method).done(function () {
-    for (let i = 0; i < response.result; i++) {
-      getProduct(response.result[i]._id)
+  return {
+    'init': function (StoreId, Logger) {
+      storeId = StoreId
+      if (typeof Logger === 'object' && Logger.hasOwnProperty('log') && Logger.hasOwnProperty('error')) {
+        logger = Logger
+      } else {
+        logger = console
+      }
+    },
+
+    // return current store ID in use
+    'storeId': storeId,
+
+    // Function to get ID product by sku
+    'getProductBySku': function (sku) {
+      let endpoint = '/products.json?sku=' + sku
+      let method = 'GET'
+      let response = runMethod(endpoint, method).done(function () {
+        for (let i = 0; i < response.result; i++) {
+          getProduct(response.result[i]._id)
+        }
+      })
+    },
+
+    // Function to get Product by ID product
+    'getProduct': function (id, callback) {
+      runMethod(callback, '/products/' + id + '.json')
+    },
+
+    'getOrder': function (id, callback) {
+      let endpoint = '/orders/' + id + '.json'
+      let method = 'GET'
+      callback = function () {
+        runMethod(endpoint, method)
+      }
     }
-  })
-}
-
-// Function to get Product by ID product
-function getProduct (id, callback) {
-  runMethod(callback, '/products/' + id + '.json')
-}
-
-function getOrder (id, callback) {
-  let endpoint = '/orders/' + id + '.json'
-  let method = 'GET'
-  callback = function () {
-    runMethod(endpoint, method)
   }
+}()
+
+if (isNodeJs) {
+  module.exports = EcomIo
 }
 
 // function getProductbyName (term) {
