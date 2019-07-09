46b6ef9bb1095908b25b540b06408102adacfcc0
Leonardo Matos <leomp120894@gmail.com>
2019-07-09 15:52:03 -0300

    feat(axios): create custom axios instance

---
+++ src/lib/axios.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+// axios HTTP client
+// https://github.com/axios/axios
+// create an instance using the config defaults provided by the library
+const instance = axios.create({
+  // up to 60s timeout
+  timeout: 60000
+})
+// always JSON for request with body data
+;[ 'post', 'patch', 'put' ].forEach(method => {
+  instance.defaults.headers[method]['Content-Type'] = 'application/json'
+})
+
+export default instance
