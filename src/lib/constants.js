1741906f0208168cc6915a9489fcea2c69d3f5e1
Leonardo Matos <leomp120894@gmail.com>
2019-07-20 14:50:09 -0300

    fix(constants): removing ':id' from API_PASSPORT

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -2,7 +2,7 @@ export const IS_BROWSER = Boolean(typeof window === 'object' && window !== null 
 
 export const API_STORE = 'https://api.e-com.plus/v1/'
 export const API_STORE_CACHE = 'https://ioapi.ecvol.com/:id/v1/'
-export const API_PASSPORT = 'https://passport.e-com.plus/v1/:id/'
+export const API_PASSPORT = 'https://passport.e-com.plus/v1/'
 export const API_SEARCH = 'https://apx-search.e-com.plus/api/v1/'
 export const API_MODULES = 'https://apx-mods.e-com.plus/api/v1/'
 export const API_STOREFRONT = 'https://iostorefront.ecvol.com/api/v1/'
